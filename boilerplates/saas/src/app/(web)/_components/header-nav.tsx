121905ec5b5bebbc1b7fe9bdabbfd9b95d73cacf
Ali Farooq <alidotm.me@gmail.com>
2024-02-07 17:17:23 +0500

    saas: Refactor header navigation component

--- boilerplates/saas/src/app/(web)/_components/header-nav.tsx
+++ boilerplates/saas/src/app/(web)/_components/header-nav.tsx
@@ -3,44 +3,48 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Badge } from "@/components/ui/badge";
-import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import ThemeToggle from "@/components/theme-toggle";
+import { webConfig } from "@/config/nav";
 
 export default function WebHeaderNav() {
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  return (
-    <nav className="flex items-center justify-center">
-      <ul className="flex items-center space-x-3">
-        {siteConfig.headerNav.map((item) => (
-          <li key={item.id}>
-            <Link
-              href={item.href}
-              className={cn(
-                "px-2 text-sm font-medium hover:underline hover:underline-offset-4",
-                isActive(item.href, pathname)
-                  ? "underline underline-offset-4"
-                  : "",
-              )}
-            >
-              <span>{item.label}</span>
-              {item.badge && (
-                <Badge variant="secondary" size="sm" className="ml-2">
-                  {item.badge}
-                </Badge>
-              )}
-            </Link>
-          </li>
-        ))}
+    return (
+        <nav className="flex items-center justify-center">
+            <ul className="flex items-center space-x-3">
+                {webConfig.headerNav.map((item) => (
+                    <li key={item.id}>
+                        <Link
+                            href={item.href}
+                            className={cn(
+                                "px-2 text-sm font-medium hover:underline hover:underline-offset-4",
+                                isActive(item.href, pathname)
+                                    ? "underline underline-offset-4"
+                                    : "",
+                            )}
+                        >
+                            <span>{item.label}</span>
+                            {item.badge && (
+                                <Badge
+                                    variant="secondary"
+                                    size="sm"
+                                    className="ml-2"
+                                >
+                                    {item.badge}
+                                </Badge>
+                            )}
+                        </Link>
+                    </li>
+                ))}
 
-        <ThemeToggle />
-      </ul>
-    </nav>
-  );
+                <ThemeToggle />
+            </ul>
+        </nav>
+    );
 }
 
 // it tells you if the current link is active or not based on the pathname
 function isActive(href: string, pathname: string) {
-  return pathname === href;
+    return pathname === href;
 }
