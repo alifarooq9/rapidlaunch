aed6cd5dbdcd4b9a456912f7ae4adc8440a54a62
Ali Farooq <alidotm.me@gmail.com>
2024-02-07 15:58:37 +0500

    Refactor layout and global styles

--- boilerplates/saas/src/app/layout.tsx
+++ boilerplates/saas/src/app/layout.tsx
@@ -4,30 +4,30 @@ import { TRPCReactProvider } from "@/trpc/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
-  subsets: ["latin"],
+    subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "RapidLaunch - Next.js Boilerplate",
-  description:
-    "Next.js boilerplate with shadcn ui, TRPC, TailwindCSS, and Drizzle.",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+    title: "RapidLaunch - Next.js Boilerplate",
+    description:
+        "Next.js boilerplate with shadcn ui, TRPC, TailwindCSS, and Drizzle.",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <TRPCReactProvider>
-          <ThemeProvider attribute="class" defaultTheme="system">
-            {children}
-          </ThemeProvider>
-        </TRPCReactProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className}`}>
+                <TRPCReactProvider>
+                    <ThemeProvider attribute="class" defaultTheme="system">
+                        {children}
+                    </ThemeProvider>
+                </TRPCReactProvider>
+            </body>
+        </html>
+    );
 }
