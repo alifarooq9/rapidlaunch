a9e99ec521bca9531598a1c1f840c88ce1b77117
Ali Farooq <Alimuhammadfc5@gmail.com>
2024-04-30 01:18:02 +0500

    feat(saas): Basic Charts UI using recharts

--- starterkits/saas/src/app/(app)/admin/dashboard/page.tsx
+++ starterkits/saas/src/app/(app)/admin/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import { AppPageShell } from "@/app/(app)/_components/page-shell";
+import { Chart } from "@/app/(app)/admin/dashboard/_components/chart";
 import { StatsCard } from "@/app/(app)/admin/dashboard/_components/stats-card";
 import { adminDashConfig } from "@/app/(app)/admin/dashboard/_constants/page-config";
 import { buttonVariants } from "@/components/ui/button";
 import { siteUrls } from "@/config/urls";
+import { cn } from "@/lib/utils";
 import { DollarSignIcon, Users2Icon } from "lucide-react";
 import Link from "next/link";
 
@@ -13,15 +15,18 @@ export default async function AdminDashPage() {
             description={adminDashConfig.description}
         >
             <div className="grid w-full gap-8">
-                <p className="font text-sm">
+                <p className="text-sm">
                     This a simple dashboard with Analytics, to see detailed
                     Analytics go to{" "}
                     <Link
                         href={siteUrls.admin.analytics}
-                        className={buttonVariants({
-                            variant: "link",
-                            className: "px-0 underline",
-                        })}
+                        className={cn(
+                            buttonVariants({
+                                variant: "link",
+                                size: "default",
+                                className: "px-0 underline",
+                            }),
+                        )}
                     >
                         PostHog Dashboard
                     </Link>
@@ -48,6 +53,11 @@ export default async function AdminDashPage() {
                         Icon={DollarSignIcon}
                         subText="+20.1% from last month"
                     />
+                </div>
+
+                <div className="grid grid-cols-2 gap-4">
+                    <Chart />
+                    <Chart />
                 </div>
             </div>
         </AppPageShell>
