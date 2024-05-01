c6d989e2800e8bfd867579133bf01ef1945595e5
Ali Farooq <Alimuhammadfc5@gmail.com>
2024-05-01 16:27:31 +0500

    refactor(saas): Card chart

--- starterkits/saas/src/app/(app)/admin/dashboard/_components/users-chart.tsx
+++ starterkits/saas/src/app/(app)/admin/dashboard/_components/users-chart.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { LineChart } from "@/components/charts";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatDate, thousandToK } from "@/lib/utils";
 
 type UsersChartProps = {
@@ -9,24 +10,34 @@ type UsersChartProps = {
 
 export function UsersChart({ data }: UsersChartProps) {
     return (
-        <LineChart
-            data={data}
-            xAxisDataKey="Date"
-            yAxisDataKey="Users"
-            lineDataKeys={["Users", "Active Users"]}
-            lineProps={[{ stroke: "hsl(var(--primary))" }, { stroke: "green" }]}
-            yAxisProps={{
-                tickFormatter: (value) => {
-                    if (value >= 10000) {
-                        return `${thousandToK(Number(value)).toFixed(1)}k`;
-                    } else {
-                        return value as string;
-                    }
-                },
-            }}
-            xAxisProps={{
-                tickFormatter: formatDate,
-            }}
-        />
+        <Card>
+            <CardHeader>
+                <CardTitle>Users Analytics</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <LineChart
+                    data={data}
+                    xAxisDataKey="Date"
+                    yAxisDataKey="Users"
+                    lineDataKeys={["Users", "Active Users"]}
+                    lineProps={[
+                        { stroke: "hsl(var(--primary))" },
+                        { stroke: "green" },
+                    ]}
+                    yAxisProps={{
+                        tickFormatter: (value) => {
+                            if (value >= 10000) {
+                                return `${thousandToK(Number(value)).toFixed(1)}k`;
+                            } else {
+                                return value as string;
+                            }
+                        },
+                    }}
+                    xAxisProps={{
+                        tickFormatter: formatDate,
+                    }}
+                />
+            </CardContent>
+        </Card>
     );
 }
