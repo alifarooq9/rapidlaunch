5b9a54009ec0a1f838f7795e1e9d8c44aaf99955
Ali Farooq <Alimuhammadfc5@gmail.com>
2024-05-06 18:50:09 +0500

    feat(saas): Organization pages mobile responsive

--- starterkits/saas/src/app/(app)/_components/data-table-pagination.tsx
+++ starterkits/saas/src/app/(app)/_components/data-table-pagination.tsx
@@ -30,7 +30,7 @@ export function DataTablePagination<TData>({
         <div className="flex w-full flex-col items-center justify-between gap-4 overflow-auto px-2 py-1 sm:flex-row sm:gap-8">
             <p className="flex-1 text-sm text-muted-foreground">
                 {totalRows}
-                <span className="hidden lg:inline">&nbsp;result(s) found</span>
+                <span className="inline">&nbsp;result(s) found</span>
             </p>
             <div className="flex flex-col items-center gap-4 sm:flex-row sm:gap-6 lg:gap-8">
                 <div className="flex items-center space-x-2">
