bde68facf55aa329e0ca0580677ab36ed015dc2c
Ali Farooq <Alimuhammadfc5@gmail.com>
2024-05-15 21:44:49 +0500

    feat(saas): Rewapping www ui

---
+++ apps/www/src/app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import { MarketingLayout } from "@/components/marketing-layout";
+
+export default function HomePage() {
+    return (
+        <MarketingLayout>
+            <h1>Marketing Page</h1>
+        </MarketingLayout>
+    );
+}
