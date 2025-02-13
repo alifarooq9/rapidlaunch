eea99f035bd1d6d4ac8751cfc856ac3878165638
Ali Farooq <alidotm.me@gmail.com>
2025-02-13 14:02:27 +0500

    updated email

--- starterkits/saas/src/config/site.ts
+++ starterkits/saas/src/config/site.ts
@@ -10,6 +10,6 @@ export const siteConfig = {
         "Get your startup off the ground quickly with RapidLaunch! This open source Next.js starter kit provides the foundation you need to build your MVP fast – pre-built components, optimized performance, and ready-to-go styling",
     orgImage:
         "https://utfs.io/f/4ae0ddb1-4260-46f5-aa7c-70408cc192b9-aadavt.png",
-    contactEmail: "hello@support.rapidlaunch.xyz",
-    noReplyEmail: "Rapidlaunch@support.rapidlaunch.xyz",
+    contactEmail: "hello@support.launchmvpfast.com",
+    noReplyEmail: "Rapidlaunch@support.launchmvpfast.com",
 } as const;
